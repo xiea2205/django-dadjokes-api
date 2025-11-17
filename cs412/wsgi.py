c0c247898f3698ed99a8761a27cacc45e5be1961
unknown <xiea@bu.edu>
2025-11-16 20:02:11 -0500

    Rename project from dadjokes_project to cs412 for BU deployment

--- cs412/wsgi.py
+++ cs412/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dadjokes_project project.
+WSGI config for cs412 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dadjokes_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs412.settings')
 
 application = get_wsgi_application()
